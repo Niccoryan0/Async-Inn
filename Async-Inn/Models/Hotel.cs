7d2929662a9a381232463e61dce3261d7466c502
Nicco Ryan <niccoryan0@gmail.com>
2020-07-21 10:38:02 -0700

    Scaffolding

---
+++ Async-Inn/Models/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Author: *Nicco Ryan*\
 Date: *7/20/2020*\
 ![ERD Diagram](Assets/Lab11-Databases.png)\
 [Diagram Descriptions](https://docs.google.com/document/d/1nppbXbjYCOY2yeuyXozYDU2KQVrGXcFB8_k19UAyZWs/edit)\
-Partners: *Bade Habib, and Bryant Davis*
+Collaborators: *Bade Habib, and Bryant Davis*
